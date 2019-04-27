e5c8e5a023ad62db4ab562010b3765b217c9cb55
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2019-04-27 21:11:25 +0100

    fixed deprecated erlang:get_stacktrace() call

--- include/gproc.hrl
+++ include/gproc.hrl
@@ -20,3 +20,12 @@
 %% gproc.hrl: Common definitions
 
 -define(TAB, gproc).
+
+
+-ifdef(OTP_RELEASE). %% => OTP is 21 or higher
+-define(EXCEPTION(Class, Reason, Stacktrace), Class:Reason:Stacktrace).
+-define(STACKTRACE(Stacktrace), Stacktrace).
+-else.
+-define(EXCEPTION(Class, Reason, _), Class:Reason).
+-define(STACKTRACE(_), erlang:get_stacktrace()).
+-endif.
